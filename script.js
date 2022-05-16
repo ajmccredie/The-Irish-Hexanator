ce4bd921b5fa79104b7be11662aad272f55d1bed
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-05-16 15:40:48 +0000

    add three sections with corresponding buttons and output elements in each; and ids for all button and output elements in the index.js file

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 let firstButton = document.getElementById('firstButton');
+
+let firstOutput = document.getElementById('firstOutput');
+
+let secondButton = document.getElementById('secondButton');
+
+let secondOutput = document.getElementById('secondOutput');
+
+let thirdButton = document.getElementById('thirdButton');
+
+let thirdOutput = document.getElementById('thirdOutput')
+
