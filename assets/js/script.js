8e143c3ad4daeed7b2d23c1fbaf18866618d1053
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-05-18 14:29:19 +0000

    add comments to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+//function for inputting name
+
+//create a list of phrases
+
+//function for maths.random to give 3 seperate results
+
+//function for splice or slice to remove used results from list of phrases
+
+//function that takes the three results and adds them together
+
+//function to reload or reset the page
+
+
 let firstButton = document.getElementById('firstButton');
 
 let firstOutput = document.getElementById('firstOutput');
