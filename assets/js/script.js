f7ee2b1e40e42ce99ee10f98e7a14fdfc0adbdea
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-05-20 14:17:57 +0000

    add arrays and functions and comment out two new arrays to fix bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,46 @@
 //function for inputting name
 
-let name = prompt();
-
-
 //function for maths.random to use 3 arrays to give three results
 
 function gentext() {
     
-    let phraseListOne = ["zero", "one", "two", "three", "four", "five","six", "seven", "eight", "nine", "ten", "eleven", "twelve"];
+    let phraseListOne = ["is a", "has always been a", "will never not be a", "smells like a", "looks like a", "sounds like a"];
 
     let firstPhrase = document.getElementById('firstResult');
 
     firstPhrase.innerHTML = phraseListOne[Math.floor(Math.random()*phraseListOne.length)];
 }
 
+//function needed for two more buttons
+
+/*function gentext() {
+    
+    let phraseListTwo = ["massive", "no good", "low down", "stinky", "unbearable", "foolish"];
+
+    let secondPhrase = document.getElementById('secondResult');
+
+    secondPhrase.innerHTML = phraseListTwo[Math.floor(Math.random()*phraseListTwo.length)];
+}
+
+function gentext() {
+    
+    let phraseListThree = ["eejit", "son of a gun", "bold boy", "party pooper", "moaner", "silly billy"];
+
+    let thirdPhrase = document.getElementById('thirdResult');
+
+    thirdPhrase.innerHTML = phraseLisThree[Math.floor(Math.random()*phraseListThree.length)];
+}*/
+
 
 //function needed for splice or slice to remove previously used results from list of phrases or maybe use three speperate arrays
 
 //function needed that takes the three results and adds them together
 
-function add(firstPhrase, secondPhrase, thirdPhrase) {
-    return firstPhrase+secondPhrase+thirdPhrase;   
-}
+    /*function add(firstPhrase, secondPhrase, thirdPhrase) {
+        return firstPhrase+secondPhrase+thirdPhrase;   
+    }
 
-alert(add (0+1+2));
+    alert(add (0+1+2));*/
 
 
 //function needed to reload or reset the page
