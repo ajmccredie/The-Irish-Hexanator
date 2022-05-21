1acb5aa7a6b169a2a51bb35607630ac8bc427786
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-05-21 10:36:05 +0000

    add different function names for each array and button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 //function for maths.random to use 3 arrays to give three results
 
-function gentext() {
+function gentextOne() {
     
     let phraseListOne = ["is a", "has always been a", "will never not be a", "smells like a", "looks like a", "sounds like a"];
 
@@ -11,26 +11,32 @@ function gentext() {
     firstPhrase.innerHTML = phraseListOne[Math.floor(Math.random()*phraseListOne.length)];
 }
 
+gentextOne()
+
+
 //function needed for two more buttons
 
-/*function gentext() {
+function gentextTwo() {
     
-    let phraseListTwo = ["massive", "no good", "low down", "stinky", "unbearable", "foolish"];
+    let phraseListTwo = ["massive", "no good", "low down", "stinky", "ridiculous", "foolish"];
 
     let secondPhrase = document.getElementById('secondResult');
 
     secondPhrase.innerHTML = phraseListTwo[Math.floor(Math.random()*phraseListTwo.length)];
 }
 
-function gentext() {
+gentextTwo()
+
+function gentextThree() {
     
     let phraseListThree = ["eejit", "son of a gun", "bold boy", "party pooper", "moaner", "silly billy"];
 
     let thirdPhrase = document.getElementById('thirdResult');
 
-    thirdPhrase.innerHTML = phraseLisThree[Math.floor(Math.random()*phraseListThree.length)];
-}*/
+    thirdPhrase.innerHTML = phraseListThree[Math.floor(Math.random()*phraseListThree.length)];
+}
 
+gentextThree()
 
 //function needed for splice or slice to remove previously used results from list of phrases or maybe use three speperate arrays
 
